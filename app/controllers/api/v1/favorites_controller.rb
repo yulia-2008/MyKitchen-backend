c4140d53fdd34a9b24abe5f32c07a657e1d46612
yulia <juliana.ny2008@gmail.com>
2020-09-11 11:03:06 -0400

    adding show and destroy methods to favorite controller

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -6,9 +6,19 @@ class Api::V1::FavoritesController < ApplicationController
           render json: @favorites
         end
 
+        def show 
+          @favorite = Favorite.find_by(id:params[:id])
+          render json: @favorite
+        end
+
         def create
           @favorite = Favorite.create(favorite_params)
             render json: { favorite: FavoriteSerializer.new(@favorite), jwt: @token }, status: :created
+        end
+
+        def destroy 
+          @favorite = Favorite.find_by(id:params[:id])
+          @favorite.destroy
         end
        
         private
